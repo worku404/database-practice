6ffa64e1d4ee6bb63e29a2c17622c13a77260357
worku404 <worku.wondoson@aastustudent.edu.et>
2026-04-01 14:12:43 +0300

    ALTER TABLE TO INCLUDE INTEGRITY CONSTRAINT

--- ddl_practice.sql
+++ ddl_practice.sql
@@ -31,16 +31,18 @@ DROP TABLE IF EXISTS Users;
 CREATE DATABASE IF NOT EXISTS ecommerce_db;
 USE ecommerce_db;
 
-#SECTION 2: TABLE CREATION
 
-# Purpose : define the structure of our core entities.
-# each table includes column definition with datatypes and column lever constraints
+-- ############################################################
+-- #SECTION 2: TABLE CREATION
+
+-- # Purpose : define the structure of our core entities.
+-- # each table includes column definition with datatypes and column lever constraints
 
 
 
 
-# Table : Users
-# Stores information about customers and administrators
+-- # Table : Users
+-- # Stores information about customers and administrators
 
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -55,19 +57,19 @@ CREATE TABLE Users (
 );
 
 
-# TABLE : Categories 
-# Organizes products into different groups (e.g electornic, food)
+-- # TABLE : Categories 
+-- # Organizes products into different groups (e.g electornic, food)
 
 CREATE TABLE Categories (
     category_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200) NOT NULL UNIQUE,
-    description TEXT,
+    description TEXT
 );
 
 
 
-# TABLE: Products
-# detail information about items available for sale
+-- # TABLE: Products
+-- # detail information about items available for sale
 
 CREATE TABLE Products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -78,9 +80,7 @@ CREATE TABLE Products (
     image_url VARCHAR(255),
     category_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-    FOREIGN KEY category_id REFERENCES Categories(category_id)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 
@@ -116,7 +116,7 @@ CREATE TABLE Orders (
     total_amount DECIMAL(10, 2) NOT NULL,
     
     status VARCHAR(10) DEFAULT 'pending',
-    shipping_address_id INT
+    shipping_address_id INT,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
@@ -136,3 +136,40 @@ CREATE TABLE Order_Items(
 
     UNIQUE (order_id, product_id)
 );
+
+
+##########################################################
+
+# SECTION 3: 
+# Pursose: Establish relationships between tables , ensureing referential integrity.
+# Addign them here after all tables are created 
+
+ALTER TABLE products
+ADD CONSTRAINT fk_product_category
+FOREIGN KEY (category_id) REFERENCES categories(category_id)
+ON DELETE RESTRICT 
+ON UPDATE CASCADE;
+
+-- Add foreign key to addresses table referencing  users
+ALTER TABLE addresses
+ADD CONSTRAINT fk_address_user
+FOREIGN KEY (user_id) REFERENCES users(user_id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- add foreign key to orders table referencing users
+ALTER TABLE Orders
+ADD CONSTRAINT fk_order_user
+FOREIGN KEY (user_id) REFERENCES users(user_id)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+-- aDD FOREIGN KEY TO ORDERS TALE REFERENCING SHIPPING ADDRESS
+ALTER TABLE orders
+ADD CONSTRAINT fk_order_shipping_address
+FOREIGN KEY (shipping_address_id) REFERENCES addresses(address_id)
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_order_item_order
+FOREIGN KEY (order_id) REFERENCES orders(order_id)
+ON DELETE CASCADE ON UPDATE CASCADE;
