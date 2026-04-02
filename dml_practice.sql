d5a089a2030240f001474a1a9f6752c357c38fdf
worku404 <worku.wondoson@aastustudent.edu.et>
2026-04-02 10:13:08 +0300

    intial commit

---
+++ dml_practice.sql
@@ -0,0 +1,19 @@
+# E-commerce Database DML Oprations Script
+
+#Author: Gold/ Gemini 2.5 flesh boom shakalaka
+# Date 2026-04-02
+
+# Description : this script demonstrates various DML commands 
+    -- SELECT
+    -- INSERT
+    -- UPDATE
+    -- DELETE
+# for e-commerce database schema. It covers basic operations, filtering, sorting , joins, aggregations,  subqueries and TRANSACTION
+
+
+# Prerequisite: the ddl_practice.sql script must have been excuted successfully to set up the database structure and intial data.ABORT
+
+# let's start
+
+USE ecommerce_db;
+
